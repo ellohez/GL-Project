4e09adecc8e98bf59f02571d6f505ee649063855
ellohez <helenpjobs@gmail.com>
2023-07-20 17:31:46 +0100

    Better store integration test

--- src/components/SignUpPage/UsernamePage/index.test.tsx
+++ src/components/SignUpPage/UsernamePage/index.test.tsx
@@ -1,4 +1,4 @@
-import { screen } from "@testing-library/react";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 
 import UsernamePage from ".";
 import { renderComponent } from "../../../../test/helpers/render";
@@ -7,16 +7,18 @@ import { formTitles } from "../../SignUpPage/index";
 describe("Username Page", () => {
   const id = formTitles[1];
   describe("Todo tests", () => {
+    // Can the validation messages be tested here? Not sure the child component is rendered.
     it.todo("Should initally load with 3 'isError' validation messages");
     it.todo("Should initially include a cross mark on each validation message");
-    it.todo(
-      "Should enable the next button when a valid email is entered: test@test.co.uk"
-    );
     it.todo(
       "Should include 3 non 'isError' validation messages when a valid email is entered"
     );
     it.todo(
       "Should show a tick mark on each validation message when a valid email is entered"
+    );
+
+    it.todo(
+      "Should enable the next button when a valid email is entered: test@test.co.uk"
     );
     it.todo(
       "Should not enable the next button if the email does not contain an @ symbol"
@@ -24,64 +26,37 @@ describe("Username Page", () => {
   });
 
   describe("renders as we'd expect", () => {
-    it("contains the input and error messages", () => {
+    it("contains the title and input", () => {
       renderComponent(<UsernamePage id={id} />);
 
+      expect(screen.findByText("Your email address"));
       expect(screen.getByTestId("email")).toBeInTheDocument();
+      // Cannot test the output of error messages - this is tested in <ValidationChecklist />
     });
   });
 
-  // TODO: Get these tests working!
-  //describe("valid user input", () => {
-  //   it("sets the page to valid on valid email entry", () => {
-  //     // const id = formTitles[1];
-  //     const { store } = renderComponent(<UsernamePage id={id} />);
-  //     const actionSpy = jest.spyOn(store, "dispatch");
+  describe("interaction with store", () => {
+    it("should update the store with the email", () => {
+      const { store } = renderComponent(<UsernamePage id={id} />);
+      fireEvent.input(screen.getByTestId("email"), {
+        target: { value: "test@test.com" },
+      });
+      const selectEmail = store.getState().newUser.email;
+      expect(selectEmail).toBe("test@test.com");
+    });
 
-  //     fireEvent.input(screen.getByTestId("email"), {
-  //       target: { value: "test@test.com" },
-  //     });
+    // it("should update the isValid flag in the store", () => {
+    //   const { store } = renderComponent(<UsernamePage id={id} />);
 
-  //     expect(actionSpy).toHaveBeenLastCalledWith(
-  //       {
-  //         type: "signUpPages/setValidTrue(id)",
-  //         payload: "test@test.com",
-  //       },
-  //       expect.objectContaining({
-  //         pageId: id,
-  //       })
-  //     );
-  //   });
-  // });
+    //   fireEvent.input(screen.getByTestId("email"), {
+    //     target: { value: "test@test.com" },
+    //   });
 
-  // describe("interaction", () => {
-  //   it("entering an email address triggers a 'setEmail' action", () => {
-  //     const { store } = renderComponent(<UsernamePage id={formTitles[1]} />);
-  //     const actionSpy = jest.spyOn(store, "dispatch");
-
-  //     fireEvent.input(screen.getByTestId("email"), {
-  //       target: { value: "test@test.com" },
-  //     });
-
-  //     // expect(actionSpy).toHaveBeenCalledWith({
-  //     //   type: "newUser/setEmail",
-  //     //   payload: "test@test.com",
-  //     // });
-  //     expect(actionSpy).toBeCalledWith(
-  //       {
-  //         type: "newUser/setEmail",
-  //         payload: "test@test.com",
-  //       },
-  //       {
-  //         type: "signUpPages/addMessage",
-  //         payload:
-  //           expect.objectContaining(
-  //             {
-  //               pageId: id,
-  //             }
-  //           )
-  //       }
-  //     )
-  //   });
-  // });
+    //   // waitFor(expect(store.getState().signUpPages.pages[1]).toBeDefined);
+    //   // const selectIsValid = store.getState().signUpPages.pages[1].isValid;
+    //   // expect(selectIsValid).toBe(true);
+    //   const updatedState = store.getState();
+    //   expect(updatedState.signUpPages.pages[1]).toEqual(true);
+    // });
+  });
 });
