cc4d04e6beb156418364857cc74f3c6cb721d71e
ellohez <helenpjobs@gmail.com>
2023-07-26 16:28:57 +0100

    Router refactor - working

--- src/components/SignUpPage/index.tsx
+++ src/components/SignUpPage/index.tsx
@@ -1,12 +1,10 @@
-import { current } from "@reduxjs/toolkit";
-import React, { useEffect, useState } from "react";
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 import { PageRoutes } from "../../router";
 import { useAppSelector } from "../../store";
 import { selectIsValid } from "../../store/signUpPages/selectors";
 import BreadcrumbTrail from "../common/BreadcrumbTrail";
-// import PageDisplay from "./PageDisplay";
 import "./styles.css";
 
 // TODO: Remove this once pages added to router?
@@ -19,9 +17,6 @@ export const formTitles: Array<string> = [
 ];
 
 const SignUpPage = (): React.JSX.Element => {
-  // To attach next button to the router.
-  const navigate = useNavigate();
-
   // Use Enum values from router - for list of routes
   // Sign up Page has Guidance page as the default (index) element so no direct path to GuidancePage needed
   const pages: string[] = [
@@ -30,14 +25,33 @@ const SignUpPage = (): React.JSX.Element => {
     PageRoutes.PasswordPage,
   ];
 
-  // Store the current page the user will view next
-  const [page, setPage] = useState(0);
-  const lastPage = formTitles.length - 1;
-  const pageIsValid = useAppSelector(selectIsValid(formTitles[page]));
+  // const location = useLocation();
+  // Find index of last '/' in pathname use string.substring(string.lastIndexOf("/"))
+  // const indexToSplitBy = location.pathname.lastIndexOf("/");
+  // let pageId: string = location.pathname.substring(indexToSplitBy);
+  // pageId = pageId.replace("/", "").trim();
+  // let linkNextPage = pages[1]; // Initialise to 1st page after guidance page which is the index page
+  // useEffect(() => {
+  //   //setPage(page + 1);
+  //   linkNextPage = pages[currentPage + 1];
+  // }, [pageIsValid]);
+  // const lastPage = formTitles.length - 1;
 
-  useEffect(() => {
-    setPage(page + 1);
-  }, []);
+  // Store the current page the user will view next
+  const [currentPage, setCurrentPage] = useState(0);
+  const lastPage = pages.length - 1;
+  const pageIsValid = useAppSelector(selectIsValid(formTitles[currentPage]));
+  const navigate = useNavigate();
+
+  const onPrevious = () => {
+    setCurrentPage(currentPage - 1);
+    navigate(-1);
+  };
+
+  const onNext = () => {
+    navigate(pages[currentPage + 1]);
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <main>
@@ -48,7 +62,7 @@ const SignUpPage = (): React.JSX.Element => {
       {/* TODO: Add CSS padding to make the <br> tags unneccesary? */}
       <br />
       {/* Draw breadcrumb trail, showing where the user is up to */}
-      <BreadcrumbTrail currentStep={page} />
+      <BreadcrumbTrail currentStep={currentPage} />
       <br />
 
       {/* Output the header and page content for the step the user is currently at */}
@@ -56,7 +70,7 @@ const SignUpPage = (): React.JSX.Element => {
         <div className="header">
           {/* Display the relevant title for the current page */}
           {/* TODO: Make each page responsible for it's own title */}
-          <h2>{formTitles[page]}</h2>
+          <h2>{formTitles[currentPage]}</h2>
         </div>
         <div className="separator"></div>
         {/* <PageDisplay page={page} /> */}
@@ -65,32 +79,34 @@ const SignUpPage = (): React.JSX.Element => {
         <Outlet />
       </div>
 
-      {/* Buttons will be controlled here, not via the individual pages */}
+      {/* Buttons will be controlled here, not via the individual pages 
+        using valid status from Redux set by the inner page. */}
+      {/* <NavigationButtons id={pageId} /> */}
       <div className="button-row">
         <button
           className="form-button h4-style"
-          // Aria-disabled attibute not needed if disabled attribute included
-          disabled={page === 0}
+          // Aria-disabled attribute not needed if disabled attribute included
+          disabled={currentPage === 0}
           // No need to add Aria role of 'button' if button has type='button'
           type="button"
-          onClick={() => {
-            setPage((currentPg: number) => currentPg - 1);
-          }}
+          // onClick={() => {
+          //   setPage((currentPg: number) => currentPg - 1);
+          // }}
+          onClick={onPrevious}
         >
           Previous
         </button>
-        <NavLink to={pages[page + 1]}>
-          <button
-            className="form-button h4-style"
-            type="button"
-            disabled={!pageIsValid}
-            // onClick={() => {
-            //   setPage((currentPg: number) => currentPg + 1);
-            // }}
-          >
-            {page === lastPage ? "Submit" : "Next"}
-          </button>
-        </NavLink>
+        <button
+          className="form-button h4-style"
+          type="button"
+          disabled={!pageIsValid}
+          // onClick={() => {
+          //   setPage((currentPg: number) => currentPg + 1);
+          // }}
+          onClick={onNext}
+        >
+          {currentPage === lastPage ? "Submit" : "Next"}
+        </button>
       </div>
     </main>
   );
