0aebdd840ccdcea45a7b816c40253cddc1473c9c
ellohez <helenpjobs@gmail.com>
2023-08-01 09:31:40 +0100

    Fixed issues with headers

--- src/services/users.ts
+++ src/services/users.ts
@@ -15,19 +15,14 @@ const BASE_URL = `http://localhost:3333`;
 
 export const postUser = async (userData: NewUser) => {
   try {
-    const { data } = await axios.post<User>(
-      `${BASE_URL}/users`,
-      // {
-      //     headers: {
-      //         Accept: 'application/json',
-      //       },
-      //     data: {
-      //         userData,
-      //     },
-      // }
-      userData
-    );
-    console.log(JSON.stringify(data, null, 4));
+    const { data } = await axios.post<User>(`${BASE_URL}/users`, userData, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // TODO: Remove test code
+    // console.log(JSON.stringify(data, null, 4));
 
     return data;
   } catch (error) {
@@ -43,15 +38,13 @@ export const postUser = async (userData: NewUser) => {
 
 export const getUsers = async () => {
   try {
-    const { data } = await axios.get<User[]>(
-      `${BASE_URL}/users`
-      // {
-      //   headers: {
-      //     Accept: 'application/json',
-      //   },
-      // },
-    );
+    const { data } = await axios.get<User[]>(`${BASE_URL}/users`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
+    //TODO: Remove test code
     //console.log(JSON.stringify(data, null, 4));
     return data;
   } catch (error) {
@@ -67,14 +60,11 @@ export const getUsers = async () => {
 
 export const getUser = async (userId: number) => {
   try {
-    const { data } = await axios.get<User>(
-      `${BASE_URL}/users/${userId}`
-      // {
-      //     headers: {
-      //       Accept: 'application/json',
-      //     },
-      // },
-    );
+    const { data } = await axios.get<User>(`${BASE_URL}/users/${userId}`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     return data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
@@ -86,21 +76,3 @@ export const getUser = async (userId: number) => {
     }
   }
 };
-
-// export const getUsers = (): Promise<void | User[]> => {
-//   return fetch(`${BASE_URL}/users`)
-//     .then((res) => res.json())
-//     .then((res) => {
-//       return res as User[];
-//     })
-//     .catch((error) => console.log(`Error from getUsers - ${error}`));
-// };
-
-// export const getUser = ({ id }: { id: number }): Promise<void | User> => {
-//     return fetch(`${BASE_URL}/user/${id}`)
-//         .then<User>((res) => res.json())
-//         .then((res) => {
-//             return res;
-//         })
-//         .catch((error) => console.log(`Error from getUser - ${error}`));
-// };
