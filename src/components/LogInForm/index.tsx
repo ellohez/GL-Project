016d3b3768fe785e380ca75ce5578e9ede0a06d7
ellohez <helenpjobs@gmail.com>
2023-08-03 18:05:59 +0100

    refactor and prep for login authentication process

--- src/components/LogInForm/index.tsx
+++ src/components/LogInForm/index.tsx
@@ -3,45 +3,71 @@ import { ChangeEventHandler, FormEventHandler, useState } from "react";
 import { useAppDispatch } from "../../store";
 import { setUserFirstName, setUserLastName } from "../../store/user/userSlice";
 
+// import { NewUser } from "../../types/services";
+
 export const LogInForm = () => {
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
+  // const [firstname, setFirstName] = useState("");
+  // const [lastname, setLastName] = useState("");
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
   const dispatch = useAppDispatch();
 
-  const handleFirstNameInput: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setFirstName(e.target.value);
-  };
-
-  const handleLastNameInput: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setLastName(e.target.value);
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    dispatch(setUserFirstName(firstname));
-    dispatch(setUserLastName(lastname));
+
+    // TODO: attempt login using email and password
+
+    // TODO: If user does not exist, or sign up is incomplete
+    // - redirect to signUpPages
+
+    // dispatch(setUserFirstName(firstname));
+    // dispatch(setUserLastName(lastname));
   };
+
+  // TODO: implement a show password checkbox
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="firstname">Your first name:</label>
-      <input
-        id="firstname"
-        name="firstname"
-        onInput={handleFirstNameInput}
-        value={firstname}
-        required
-      />
-      <label htmlFor="lastname">Your last name:</label>
-      <input
-        id="lastname"
-        name="lastname"
-        onInput={handleLastNameInput}
-        value={lastname}
-        required
-      />
-
+      <fieldset>
+        <label className="help-label" htmlFor="email" id="emailLabel">
+          Your email:
+        </label>
+        <input
+          className="block-input"
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          aria-labelledby="emailLabel"
+          aria-required="true"
+          onInput={handleChange}
+          value={formData.email}
+          required
+        />
+        <label className="help-label" htmlFor="password" id="pwd-label">
+          Your password:
+        </label>
+        <input
+          className="block-input"
+          id="password"
+          name="password"
+          type="password"
+          autoComplete="password"
+          aria-labelledby="pwd-label"
+          aria-required="true"
+          onInput={handleChange}
+          value={formData.password}
+          required
+        />
+      </fieldset>
       <button type="submit">SUBMIT</button>
     </form>
   );
--- src/components/LogInPage/index.test.tsx
+++ src/components/LogInPage/index.test.tsx
@@ -8,33 +8,33 @@ describe("User Page", () => {
     it("renders how we'd expect", () => {
       renderComponent(<LogInPage />);
 
-      expect(screen.getByLabelText("Your first name:")).toBeInTheDocument();
-      expect(screen.getByLabelText("Your last name:")).toBeInTheDocument();
+      expect(screen.getByLabelText("Your email:")).toBeInTheDocument();
+      expect(screen.getByLabelText("Your password:")).toBeInTheDocument();
     });
   });
 
-  describe("interact", () => {
-    it("submitting the form triggers a 'setUsername' action", () => {
-      const { store } = renderComponent(<LogInPage />);
-      const actionSpy = jest.spyOn(store, "dispatch");
+  // describe("interact", () => {
+  //   it("submitting the form triggers a 'setUsername' action", () => {
+  //     const { store } = renderComponent(<LogInPage />);
+  //     const actionSpy = jest.spyOn(store, "dispatch");
 
-      fireEvent.input(screen.getByLabelText("Your first name:"), {
-        target: { value: "Marco" },
-      });
-      fireEvent.input(screen.getByLabelText("Your last name:"), {
-        target: { value: "Polo" },
-      });
-      fireEvent.click(screen.getByText("SUBMIT"));
+  //     fireEvent.input(screen.getByLabelText("Your first name:"), {
+  //       target: { value: "Marco" },
+  //     });
+  //     fireEvent.input(screen.getByLabelText("Your last name:"), {
+  //       target: { value: "Polo" },
+  //     });
+  //     fireEvent.click(screen.getByText("SUBMIT"));
 
-      expect(actionSpy).toBeCalledWith({
-        type: "user/setUserFirstName",
-        payload: "Marco",
-      });
+  //     expect(actionSpy).toBeCalledWith({
+  //       type: "user/setUserFirstName",
+  //       payload: "Marco",
+  //     });
 
-      expect(actionSpy).toBeCalledWith({
-        type: "user/setUserLastName",
-        payload: "Polo",
-      });
-    });
-  });
+  //     expect(actionSpy).toBeCalledWith({
+  //       type: "user/setUserLastName",
+  //       payload: "Polo",
+  //     });
+  //   });
+  // });
 });
