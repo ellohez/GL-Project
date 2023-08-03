ff89b278319a3419330abc87da028d1db6cb4ca0
ellohez <helenpjobs@gmail.com>
2023-08-03 13:48:10 +0100

    Updated for new UserSlice selectors

--- src/components/LogInForm/index.tsx
+++ src/components/LogInForm/index.tsx
@@ -1,29 +1,44 @@
 import { ChangeEventHandler, FormEventHandler, useState } from "react";
 
 import { useAppDispatch } from "../../store";
-import { setUserName } from "../../store/user/userSlice";
+import { setUserFirstName, setUserLastName } from "../../store/user/userSlice";
 
 export const LogInForm = () => {
-  const [name, setName] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+
   const dispatch = useAppDispatch();
 
-  const handleNameInput: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setName(e.target.value);
+  const handleFirstNameInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setLastName(e.target.value);
   };
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    dispatch(setUserName(name));
+    dispatch(setUserFirstName(firstname));
+    dispatch(setUserLastName(lastname));
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Your name:</label>
+      <label htmlFor="firstname">Your first name:</label>
       <input
-        id="username"
-        name="username"
-        onInput={handleNameInput}
-        value={name}
+        id="firstname"
+        name="firstname"
+        onInput={handleFirstNameInput}
+        value={firstname}
+        required
+      />
+      <label htmlFor="lastname">Your last name:</label>
+      <input
+        id="lastname"
+        name="lastname"
+        onInput={handleLastNameInput}
+        value={lastname}
         required
       />
 
