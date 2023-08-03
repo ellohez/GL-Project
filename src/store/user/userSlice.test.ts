e1e693c590528af66a62f3cb1589925abc4bc6a6
ellohez <helenpjobs@gmail.com>
2023-08-03 13:43:15 +0100

    Updated with new store selectors/reducers

--- src/store/user/userSlice.test.ts
+++ src/store/user/userSlice.test.ts
@@ -1,16 +1,21 @@
 import { createTestStore } from "../../../test/helpers/store";
-import { setUserName } from "./userSlice";
+import { setUserFirstName, setUserLastName } from "./userSlice";
 
 describe("userSlice", () => {
   describe("setUsername", () => {
     it("updates the username in the store", () => {
       const testStore = createTestStore();
 
-      testStore.dispatch(setUserName("John Smith"));
+      testStore.dispatch(setUserFirstName("John"));
+      testStore.dispatch(setUserLastName("Smith"));
 
       const updatedState = testStore.getState();
 
-      expect(updatedState.user).toEqual({ name: "John Smith" });
+      expect(updatedState.user).toEqual({
+        firstName: "John",
+        lastName: "Smith",
+        id: -1,
+      });
     });
   });
 });
