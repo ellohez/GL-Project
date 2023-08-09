5d5cb3ca009cfd618711769e86b0ef85411c91a8
ellohez <helenpjobs@gmail.com>
2023-08-09 11:57:57 +0100

    Axios method for logging user in added

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,8 +1,35 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 import { NewUser, User } from "../types/services";
 
 export const BASE_URL = `http://localhost:3333`;
+// const responseBody = (response: AxiosResponse) => response.data;
+
+// Add a new user to the DB (username and password)
+export const loginUser = async (userData: NewUser) => {
+  // try {
+  const { data } = await axios.post<string>(
+    `${BASE_URL}/login`,
+    JSON.stringify(userData),
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return data;
+  // } catch (error) {
+  //   if (axios.isAxiosError(error)) {
+  //     //console.log("postUser - error message: ", error.message);
+  //     // return error.message;
+  //     throw new Error(error);
+  //   } else {
+  //     //console.log("postUser - unexpected error: ", error);
+  //     // return "An unexpected error occurred";
+  //     throw "An unexpected error occured";
+  //   }
+  // }
+};
 
 // Add a new user to the DB (username and password)
 export const postUser = async (userData: NewUser) => {
@@ -20,14 +47,16 @@ export const postUser = async (userData: NewUser) => {
     // TODO: Remove test code
     // console.log(JSON.stringify(data, null, 4));
 
-    return JSON.parse(data);
+    return JSON.parse(data); // Should data be returned as raw?
   } catch (error) {
     if (axios.isAxiosError(error)) {
       console.log("postUser - error message: ", error.message);
-      return error.message;
+      // return error.message;
+      throw error.message;
     } else {
       console.log("postUser - unexpected error: ", error);
-      return "An unexpected error occurred";
+      // return "An unexpected error occurred";
+      throw "An unexpected error occured";
     }
   }
 };
