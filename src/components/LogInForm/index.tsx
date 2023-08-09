b93bfdd252570590c59dc32dc23bb5479b27b9f6
ellohez <helenpjobs@gmail.com>
2023-08-09 11:57:38 +0100

    API communication converted to Axios - working

--- src/components/LogInForm/index.tsx
+++ src/components/LogInForm/index.tsx
@@ -1,96 +1,90 @@
-import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import axios from "axios";
+import { ChangeEventHandler, FormEventHandler, useRef, useState } from "react";
 
 import { PageRoutes } from "../../router";
-import { useLoginUserMutation } from "../../services/authAPI";
-import { useAppDispatch, useAppSelector } from "../../store";
-import { selectSignUpComplete, selectUserId } from "../../store/user/selectors";
+import { loginUser } from "../../services/users";
+import { useAppDispatch } from "../../store";
 import {
   setSignUpComplete,
   setUserFirstName,
   setUserId,
   setUserLastName,
 } from "../../store/user/userSlice";
-import { NewUser } from "../../types/services";
 import RedirectionModal from "../common/RedirectionModal";
 
 export const LogInForm = () => {
   const dispatch = useAppDispatch();
-  const userCompletedSignup = useAppSelector(selectSignUpComplete);
-  const userId = useAppSelector(selectUserId);
-
+  const [errorMessage, setErrorMessage] = useState("");
+  const errorRef = useRef<HTMLParagraphElement>(null);
+  // User entered form data
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  // Used to trigger the RedirectionalModal appearance if user needs to
+  // complete the sign up procedure to continue.
   const [redirect, setRedirect] = useState(false);
 
-  const [loginUser, { data: loginResult, status, error, isError, isLoading }] =
-    useLoginUserMutation();
-  const attemptLogin = (user: NewUser) => {
-    // Attempt login using email and password
-    loginUser(user);
-
-    // Deal with any API related issues
-    if (isLoading) {
-      // TODO: open loader component
-    }
-    if (isError || !loginResult || error) {
-      console.error(`Login failed - Error = ${error}`);
-      const errorString = JSON.stringify(error);
-      const statusString = JSON.stringify(status);
-      const resString = JSON.stringify(loginResult);
-      alert(`Login failed - error = ${errorString} 
-                        status = ${statusString}
-                        result = ${resString}`);
-    } else {
-      alert("Login successful");
-      console.log(loginResult);
-      storeUserDetails();
-      // If user exists but has not completed the sign up procedure
-      // redirect them to do so.
-      if (userId > -1 && !userCompletedSignup) {
-        setRedirect(true);
-      }
-    }
-
-    // TODO: If user does not exist, or sign up is incomplete
-    // - redirect to signUpPages
-  };
-
-  const storeUserDetails = () => {
+  // For successful login - add user details to redux
+  const storeUserDetails = (loginResult: string) => {
     const fullUser = JSON.parse(loginResult).user;
     // TODO: handle the access token
     const accessToken = JSON.parse(loginResult).accessToken;
+    //TODO: remove test code:
+    console.log(fullUser.firstName, " ", fullUser.surname, " ", accessToken);
+
+    dispatch(setUserId(fullUser.id));
+    dispatch(setSignUpComplete(fullUser.signUpComplete));
+    // Redirect user to complete their signup if incomplete.
+    if (fullUser.id > -1 && !fullUser.signUpComplete) {
+      setRedirect(true);
+    }
+    // If user has completed the sign up procedure, store their details
     dispatch(setUserFirstName(fullUser.firstName));
     dispatch(setUserLastName(fullUser.surname));
-    dispatch(setSignUpComplete(fullUser.signUpComplete));
-    dispatch(setUserId(fullUser.id));
-    //console.log(fullUser.firstName, " ", fullUser.surname, " ", accessToken);
   };
 
+  // Store user entered data in state.
   const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    const user: NewUser = {
-      email: formData.email,
-      password: formData.password,
-    };
-    attemptLogin(user);
+
+    try {
+      const response = await loginUser({
+        email: formData.email.toLowerCase(),
+        password: formData.password,
+      });
+      // console.log(response);
+      setErrorMessage("Login Successful");
+      storeUserDetails(JSON.stringify(response));
+      setFormData({ email: "", password: "" });
+    } catch (error) {
+      console.log("Error with loginUser call");
+      if (axios.isAxiosError(error)) {
+        console.log(error.toJSON());
+        if (!error.response) {
+          setErrorMessage("Login failed - No server response");
+        } else if (error.response?.status === 400) {
+          setErrorMessage(`Login failed - user does not exist`);
+        } else if (error.response?.status !== 200) {
+          setErrorMessage(
+            `Status not equal to 200. Status = ${error.response?.status}`
+          );
+        } else {
+          setErrorMessage("Login failed");
+        }
+      } else {
+        console.log(error);
+        setErrorMessage("Login failed - ");
+      }
+    }
   };
 
-  // return (redirect) ?
-  //   (
-  //     <RedirectionModal
-  //       message={"You have not completed your sign up, you will be redirected to do this."}
-  //       redirectLink={`/${PageRoutes.SignUpPage}`}
-  //       trigger={true} />
-  //   ) :
-
   return (
-    <div>
+    <div className="outer-form">
       <RedirectionModal
         message={
           "You have not completed your sign up, you will be redirected to do this."
@@ -131,7 +125,15 @@ export const LogInForm = () => {
             value={formData.password}
             required
           />
+          <p
+            ref={errorRef}
+            className={errorMessage ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errorMessage}
+          </p>
         </fieldset>
+
         <button type="submit">SUBMIT</button>
       </form>
     </div>
