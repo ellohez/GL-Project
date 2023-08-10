5a07a34303d2b789d5a6aeb3c0eeb0300ae8bfab
ellohez <helenpjobs@gmail.com>
2023-08-10 13:40:18 +0100

    Main text changed

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -2,7 +2,7 @@ const LandingPage = () => {
   return (
     <main>
       <div className="main-container">
-        <h1>Hello, world! (h1)</h1>
+        <h1>Welcome! (h1)</h1>
         <h2>These headings... (h2)</h2>
         <h3>...are here to enable... (h3)</h3>
         <h4>...screen reader and contrast checking (h4)</h4>
