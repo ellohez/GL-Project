3b720b8fcfd2511c22d6e6b3a0a45c5bd1bd95b5
ellohez <helenpjobs@gmail.com>
2023-08-10 17:36:59 +0100

    clean up

--- src/components/SignUpPage/UsernamePage/index.tsx
+++ src/components/SignUpPage/UsernamePage/index.tsx
@@ -114,7 +114,6 @@ const UsernamePage = ({ id }: { id: string }): React.JSX.Element => {
     <section>
       <div className="header">
         {/* Display the relevant title for the current page */}
-        {/* TODO: Make each page responsible for it's own title */}
         <h2>Username</h2>
       </div>
       {/* Row of helper text, input or similar 
